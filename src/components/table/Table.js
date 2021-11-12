aeee2de6ed69b7367c687c20ffa33d69ea81f747
Dmitry <dmitry.aleksandrin@gmail.com>
2021-11-12 21:39:46 +0300

    добавил множественное выделение ячеек

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import { createTable } from './table.template'
 import { resizeHandler } from './table.resize'
-import { shouldResize } from './table.functions'
+import { isCell, shouldResize } from './table.functions'
+import { TableSelection } from './TableSelection'
+import {$} from '@core/dom'
+import { matrix } from './table.functions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -16,7 +19,40 @@ export class Table extends ExcelComponent {
     // console.log(event.target.getAttribute('data-resize'))
     if (shouldResize(event)) {
      resizeHandler(this.$root, event)
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      if (event.shiftKey) {
+        console.log('just a click', $target.id(true))
+        console.log('with shift', this.selection.current.id(true))
+        const target = $target.id(true)
+        const current = this.selection.current.id(true)
+
+
+        const $cells = matrix(target, current).map(id => this.$root.find(`[data-id="${id}"]`))
+
+        this.selection.selectGroup($cells)
+
+        
+      } else {
+        this.selection.select($target)
+      }
     }
+  }
+
+  prepare() {
+    console.log('prepare')
+    this.selection = new TableSelection()
+  }
+
+  init() {
+    super.init()
+
+    console.log('init')
+
+    // this.selection = new TableSelection()
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selection.select($cell)
+    console.log(this)
   }
 
   toHTML() {
